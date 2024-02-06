9dc8f4ab274ffc507d23577e040000a827ba322e
aarontauro <aarontauro00@gmail.com>
2024-02-06 22:35:49 +0530

    Upadate login.js

--- pages/api/login.js
+++ pages/api/login.js
@@ -2,18 +2,10 @@ import connectToMongo from "@/middleware/mongoose";
 import User from "@/models/User";
 import CryptoJS from "crypto-js";
 import jwt from "jsonwebtoken";
-import Cors from 'cors';
-
-const cors = Cors({
-    methods: ['GET', 'POST', 'OPTIONS'], // Add the methods your API route supports
-    origin: 'https://codeswear-aarontauro.vercel.app', // Replace with your actual frontend origin
-  })
 
 connectToMongo();
 
 export default async (req, res) => {
-     cors();
-
     if (req.method !== "POST") {
         return res.status(400).json({ message: "Method not allowed" });
     }
