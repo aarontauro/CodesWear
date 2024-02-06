2e0ddf04b1474c17403fac4bf52c465d43414dc1
aarontauro <aarontauro00@gmail.com>
2024-02-06 22:51:29 +0530

    Update pages\login.js

--- pages/login.js
+++ pages/login.js
@@ -22,7 +22,7 @@ const Login = () => {
     const handleSubmit = async (e) => {
         e.preventDefault()
 
-        const res = await fetch(`http://localhost:3000/api/login`, {
+        const res = await fetch(`${process.env.NEXT_PUBLIC_HOST}/api/login`, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
